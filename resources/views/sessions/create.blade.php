9d97519e3ebf56a0117afa725494370eb899294b
kkopitehong <kkopite.hong@smartcorelink.com>
2020-08-14 18:30:48 +0800

    登录是记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -21,6 +21,13 @@
               <label for="password">密码：</label>
               <input type="password" name="password" class="form-control" value="{{ old('password') }}">
             </div>
+
+            <div class="form-group">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="remeber" id="exampleCheck1">
+                <label for="exampleCheck1" class="form-check-label">记住我</label>
+              </div>
+            </div>
   
             <button type="submit" class="btn btn-primary">登录</button>
 
